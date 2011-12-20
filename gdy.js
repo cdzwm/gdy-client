43ec752d3a00c94c20142d1c4c2c3955400eedc8
canw <nodejs@live.cn>
2011-12-20 11:31:30 +0800

    use deps/comm/message.js

--- gdy.js
+++ gdy.js
@@ -2,17 +2,9 @@ global.DEBUG = (process.env.GDY_DEBUG || false) == 1;
 var net = require('net');
 var message = require("./lib/message");
 
-var msg_begin = "GDY_MSG_BEGIN{";
-var msg_end="}GDY_MSG_END";
-
-var hello_msg = {"cmd": "MSG"};
-
-var t
-	,client = net.connect(10086, '127.0.0.1', function(){
+var t;
+var client = net.connect(10086, '127.0.0.1', function(){
 	setTimeout(hello, 500);
-
-//	setTimeout(hello, 3000);
-
 	t = setTimeout(sendMessage, 1000);
 	client.setEncoding('utf8');
 	client.on("close", function(){
@@ -26,16 +18,17 @@ client.on("error", function(err){
 	if( err.code == "ECONNREFUSED")
 		console.log("无法连接服务器");
 });
+
 function hello(){
-	hello_msg.cmd =  "CONNECT";
-	client.write(msg_begin + JSON.stringify(hello_msg) + msg_end);
+	client.write(message.newMessage("CONNECT"));
 }
 function onReceiveMessage(data){
 	var mq = [];
 	message.parseMessage(data, mq);
 	if( mq[0].cmd == "CONNECT_OK" ){
+
 		login_msg = {cmd: "LOGIN", username:"gdy", password:"gdypassword"};
-		client.write(msg_begin + JSON.stringify(login_msg) + msg_end);
+		client.write(message.msgBegin() + JSON.stringify(login_msg) + message.msgEnd());
 		if( DEBUG )
 			console.log("LOGIN");
 	}
@@ -48,9 +41,7 @@ function onReceiveMessage(data){
 }
 
 function sendMessage(){
-	hello_msg.cmd =  "MSG";
-	var s = msg_begin + JSON.stringify(hello_msg) + msg_end;
 	for(var i=0;i<1;i++)
-	client.write(s);
+		client.write(message.newMessage("MSG"));
 	t = setTimeout(sendMessage, Math.round(Math.random() * 1000));
 }
